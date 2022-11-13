8ea33bd6165b8a7487f98aa197017976f6cb2afe
Samantha <109199225+Samantha-Sokolis@users.noreply.github.com>
2022-11-13 16:48:56 +1100

    all API routes updated

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # E-commerce Back End
+
+
+
+
+# Usage
+
+Run the following command at the root of your project and answer the prompted questions:
+
+mysql -u root -p
+
+Enter PW when promted
+
+source db/schema.sql
+
+quit
+
+npm run seed
+
+npm start
+
+# Resources: https://github.com/jpd61/e-commerce-backend/blob/master/routes/api/product-routes.js
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,10 +6,10 @@ const { Category, Product, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   // find all categories - included its associated Products
   try {
-    const productData = await Category.findAll({
+    const categoryData = await Category.findAll({
       include: [{ model: Category }, { model: Product }, { model: ProductTag }],
     });
-    res.status(200).json(productData);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -19,11 +19,11 @@ router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const productData = await Category.findByPk(req.params.id, {
+    const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Category }, { model: Product }, { model: ProductTag }],
     });
 
-    if (!productData) {
+    if (!categoryData) {
       res.status(404).json({ message: 'No product found with that id!' });
       return;
     }
@@ -36,7 +36,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new category
   try {
-    const productData = await Category.create({
+    const categoryData = await Category.create({
       id: req.body.id,
       category_name: req.body.category_name,
     });
@@ -49,12 +49,12 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
-    const productData = await Category.update(req.body, {
+    const categoryData = await Category.update(req.body, {
       where: {
         id: req.params.id,
       },
     });
-    if (!productData[0]) {
+    if (!categoryData[0]) {
       res.status(404).json({ message: 'No category with this id!' });
       return;
     }
